a62909b18984d32c1bc8b9052dcc3c290ea1ace3
Sinward Ye <244649399@qq.com>
2019-01-30 14:28:14 +0800

    增加了1008

---
+++ pat/甲/1008/1008.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int n;
+
+int main(){
+	int floor_on=0,floor_to=0,cost=0;
+	int i;
+	cin>>n;
+	for(i=0;i<n;i++){
+		cin>>floor_to;
+		if(floor_to>floor_on){
+			cost+=6*(floor_to-floor_on);
+		}else{
+			cost+=4*(floor_on-floor_to);
+		}
+		cost+=5;
+		floor_on=floor_to;
+	}
+	cout<<cost;
+	return 0;
+}
