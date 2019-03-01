aec3121b9b19f767554c60178003246bae871b66
Sinward <244649399@qq.com>
2019-03-01 20:57:40 +0800

    修正了1007

--- pat/A/1007/1007.cpp
+++ pat/A/1007/1007.cpp
@@ -9,13 +9,11 @@ int main(){
 	int i,j;
 	int max=INT_MIN,sum;
 	int start=-1,end=-1;
+	
 	cin>>n;
-	//cout<<"n="<<n<<endl;
 	for(i=0;i<n;i++){
 		cin>>data[i];
-		//cout<<data[i]<<' ';
 	}
-	//cout<<endl;
 	for(i=0;i<n;i++){
 		sum=0;
 		for(j=i;j<n;j++){
@@ -27,8 +25,7 @@ int main(){
 			}
 		}
 	}
-	if(max>0)cout<<max<<' '<<data[start]<<' '<<data[end]<<endl;
-	else if(max==0&&start==1) cout<<0<<' '<<data[start]<<' '<<data[end]<<endl;
+	if(max>=0)cout<<max<<' '<<data[start]<<' '<<data[end]<<endl;
 	else cout<<0<<' '<<data[0]<<' '<<data[n-1]<<endl;
 	return 0;
 }
